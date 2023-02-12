67fd124d6b34d8841270e09166f99662eb475ed4
yahoogm <yaho.garfield@gmail.com>
2023-02-12 21:39:42 +0700

    core: import components header & slide

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { Header } from './components/header/Header'
+import Slide from './components/slide/Slide'
 
-const App = (): any => {
-  return <div></div>
+const App = (): JSX.Element => {
+  return (
+    <div>
+      <Header />
+      <Slide />
+    </div>
+  )
 }
 
 export default App
